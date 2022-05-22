7d7d18eef66c9d50bf4edfa6778dd198ca27d338
Joel Hillacre <joel@403forbidden.ca>
2022-05-22 16:18:36 -0600

    refactor: v2, less pumps more perf

--- room-heater-via-exchange.s
+++ room-heater-via-exchange.s
@@ -1,9 +1,7 @@
-alias coolpumpin d0
-alias coolpumpout d1
-alias coolsensor d2
-alias warmpumpin d3
-alias warmpumpout d4
-alias warmsensor d5
+alias coolsensor d0
+alias warmpumpin d1
+alias warmpumpout d2
+alias warmsensor d3
 
 define TARGET 293.15
 define SAMPLEPRESSURE 1000
@@ -12,51 +10,29 @@ define MAXWARMPRESSURE 5000
 init:
 s coolsensor On 1
 s warmsensor On 1
-s coolpumpin Setting 100
-s coolpumpout Setting 100
 s warmpumpin Setting 1
 s warmpumpout Setting 100
 
+waittoreset:
+sleep 5
+
 reset:
-s coolpumpin On 0
-s coolpumpout On 1
 s warmpumpin On 0
 s warmpumpout On 1
-sleep 5
 
 waitforreset:
 yield
 l r0 coolsensor Pressure
-bnez r0 waitforreset
+beqz r0 waitforreset
 l r0 warmsensor Pressure
 bnez r0 waitforreset
-s coolpumpout On 0
 s warmpumpout On 0
-
-coolfill:
-s coolpumpin On 1
-s coolpumpout On 0
-s warmpumpin On 0
-s warmpumpout On 0
-
-waitforcoolfill:
-yield
-l r0 coolsensor Pressure
-ble r0 SAMPLEPRESSURE waitforcoolfill
-s coolpumpin On 0
 
 # reset if cool is warm enough
 l r0 coolsensor Temperature
-bge r0 TARGET reset
+bge r0 TARGET waittoreset
 
 warmfill:
-# overtarget Temperature by how cool it is, we
-# likely aren't heating the whole room at once.
-l r0 coolsensor Temperature
-sub r1 TARGET r0
-add r1 TARGET r1
-s coolpumpin On 0
-s coolpumpout On 0
 s warmpumpin On 1
 s warmpumpout On 0
 
@@ -65,5 +41,5 @@ yield
 l r0 warmsensor Pressure
 bge r0 MAXWARMPRESSURE reset
 l r0 coolsensor Temperature
-blt r0 r1 waitforwarmfill
+blt r0 TARGET waitforwarmfill
 j reset
