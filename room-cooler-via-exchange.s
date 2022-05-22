7aa398eac2b770ed3a73140f3e21491761ea5436
Joel Hillacre <joel@403forbidden.ca>
2022-05-22 15:08:21 -0600

    fix: overpasted cooler with heater

--- room-cooler-via-exchange.s
+++ room-cooler-via-exchange.s
@@ -5,16 +5,16 @@ alias warmpumpin d3
 alias warmpumpout d4
 alias warmsensor d5
 
-define TARGET 293.15
+define TARGET 296.15
 define SAMPLEPRESSURE 1000
-define MAXWARMPRESSURE 5000
+define MAXCOOLPRESSURE 5000
 
 init:
 s coolsensor On 1
 s warmsensor On 1
-s coolpumpin Setting 100
+s coolpumpin Setting 1
 s coolpumpout Setting 100
-s warmpumpin Setting 1
+s warmpumpin Setting 100
 s warmpumpout Setting 100
 
 reset:
@@ -33,7 +33,28 @@ bnez r0 waitforreset
 s coolpumpout On 0
 s warmpumpout On 0
 
+warmfill:
+s warmpumpin On 1
+s warmpumpout On 0
+s coolpumpin On 0
+s coolpumpout On 0
+
+waitforwarmfill:
+yield
+l r0 warmsensor Pressure
+ble r0 SAMPLEPRESSURE waitforwarmfill
+s warmpumpin On 0
+
+# reset if warm is cool enough
+l r0 warmsensor Temperature
+ble r0 TARGET reset
+
 coolfill:
+# overtarget Temperature by how warm it is
+# we likely aren't cooling the whole room at once.
+l r0 coolsensor Temperature
+sub r1 r0 TARGET
+sub r1 TARGET r1
 s coolpumpin On 1
 s coolpumpout On 0
 s warmpumpin On 0
@@ -42,28 +63,7 @@ s warmpumpout On 0
 waitforcoolfill:
 yield
 l r0 coolsensor Pressure
-ble r0 SAMPLEPRESSURE waitforcoolfill
-s coolpumpin On 0
-
-# reset if cool is warm enough
+bge r0 MAXCOOLPRESSURE reset
 l r0 coolsensor Temperature
-bge r0 TARGET reset
-
-warmfill:
-# overtarget Temperature by how cool it is, we
-# likely aren't heating the whole room at once.
-l r0 coolsensor Temperature
-sub r1 TARGET r0
-add r1 TARGET r1
-s coolpumpin On 0
-s coolpumpout On 0
-s warmpumpin On 1
-s warmpumpout On 0
-
-waitforwarmfill:
-yield
-l r0 warmsensor Pressure
-bge r0 MAXWARMPRESSURE reset
-l r0 coolsensor Temperature
-blt r0 r1 waitforwarmfill
+bgt r0 TARGET waitforcoolfill
 j reset
