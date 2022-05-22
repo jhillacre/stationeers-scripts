2f9e0b7dba45491aed7a5b1bb2b8f582ec3b469c
Joel Hillacre <joel@403forbidden.ca>
2022-05-22 17:24:57 -0600

    Update room-cooler-via-exchange.s

--- room-cooler-via-exchange.s
+++ room-cooler-via-exchange.s
@@ -3,7 +3,8 @@ alias coolpumpout d1
 alias coolsensor d2
 alias warmsensor d3
 
-define TARGET 296.15
+define TARGET 294.15
+define TOOWARM 295.15
 define SAMPLEPRESSURE 1000
 define MAXCOOLPRESSURE 5000
 
@@ -12,9 +13,6 @@ s coolsensor On 1
 s warmsensor On 1
 s coolpumpin Setting 100
 s coolpumpout Setting 100
-
-waittoreset:
-sleep 5
 
 reset:
 s coolpumpin On 0
@@ -28,9 +26,10 @@ l r0 coolsensor Pressure
 bnez r0 waitforreset
 s coolpumpout On 0
 
-# reset if warm is cool enough
+waitforwork:
+sleep 5
 l r0 warmsensor Temperature
-ble r0 TARGET waittoreset
+ble r0 TOOWARM waitforwork
 
 coolfill:
 s coolpumpin On 1
