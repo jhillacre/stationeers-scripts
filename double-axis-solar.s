3c2634a83da0d156089222ec06e5f13fb9df2d3b
Joel Hillacre <joel@403forbidden.ca>
2022-06-16 22:05:27 -0600

    feat: work with all panels, auto configure hrotate, hpark and vpark.

--- double-axis-solar.s
+++ double-axis-solar.s
@@ -1,35 +1,122 @@
 alias sensor d0
-alias panel d1
 
-alias panelhash r0
-alias daylight r1
-alias horiangle r2
-alias vertangle r3
-define hpark 180
-define vpark 90
-define hrotate 180
-define vdaystart 90
-define vdayend -90
+alias daylight r0
+alias horiangle r1
+alias vertangle r2
+alias state r3
+alias hrotate r4
+alias hpark r5
+alias vpark r6
+alias ratio r7
+alias maxratio r8
+alias maxindex r9
 
-reset:
-# make sure panel is finished, otherwise this dies.
-l panelhash panel PrefabHash
+# if the panels never move...
+# check for new prefab hashs
+define heavy -934345724
+define heavydual -1545574413
+define normal -2045627372
+define normaldual -539224550
 
-loop:
-yield
-l horiangle sensor Horizontal
-l vertangle sensor Vertical
-slt r4 vertangle vdaystart
-sgt r5 vertangle vdayend
-and daylight r4 r5
+init:
+move state 6
+move hrotate 0
+move hpark 0
+move vpark 0
+move ratio 0
+move maxratio 0
+move maxindex 0
+
+main:
+jal loadDevices
 bnez daylight day
 night:
+bnez state day
 move horiangle hpark
 move vertangle vpark
 day:
+beq state 6 waitForDay
+beq state 5 stackHRotateToRatio
+beq state 4 setHRotateByMaxRatio
+beq state 3 waitForNight
+beq state 2 waitForDay
+beq state 1 setPark
+returnToMain:
+jal setDevices
+j main
+
+stackHRotateToRatio:
+move sp 0
+move maxratio 0
+pushRatio:
+jal loadDevices
+move hrotate sp
+mul hrotate hrotate 90
+jal setDevices
+sleep 15
+lb ratio heavy Ratio Maximum
+max maxratio maxratio ratio
+lb ratio heavydual Ratio Maximum
+max maxratio maxratio ratio
+lb ratio normal Ratio Maximum
+max maxratio maxratio ratio
+lb ratio normaldual Ratio Maximum
+max maxratio maxratio ratio
+push maxratio
+blt sp 4 pushRatio
+sub state state 1
+j returnToMain
+
+setHRotateByMaxRatio:
+move maxratio 0
+findMax:
+ble sp 0 foundMax
+pop ratio
+max maxratio maxratio ratio
+beq ratio maxratio findMaxSet
+j findMax
+findMaxSet:
+move maxratio ratio
+move maxindex sp
+j findMax
+foundMax:
+mul hrotate maxindex 90
+sub state state 1
+j returnToMain
+
+waitForNight:
+bgtz daylight returnToMain
+sub state state 1
+j returnToMain
+
+waitForDay:
+blez daylight returnToMain
+sub state state 1
+j returnToMain
+
+setPark:
+move hpark horiangle
+move vpark vertangle
+sub state state 1
+j returnToMain
+
+loadDevices:
+l daylight sensor Activate
+l horiangle sensor Horizontal
+l vertangle sensor Vertical
+j ra
+
+setDevices:
 add horiangle horiangle hrotate
 sub vertangle 90 vertangle
-sb panelhash Horizontal horiangle
-sb panelhash Vertical vertangle
-sleep 1
-j loop
+sb heavy Horizontal horiangle
+sb heavy Vertical vertangle
+sb heavydual Horizontal horiangle
+sb heavydual Vertical vertangle
+sb normal Horizontal horiangle
+sb normal Vertical vertangle
+sb normaldual Horizontal horiangle
+sb normaldual Vertical vertangle
+s db Setting state
+yield
+j ra
