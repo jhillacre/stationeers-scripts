247a08df90a5bb1aecc229b34a23b7825527d0ec
Joel Hillacre <joel@403forbidden.ca>
2022-09-03 22:18:11 -0600

    fix: fit comments to ingame script editor

--- active-vent-set-controller.s
+++ active-vent-set-controller.s
@@ -1,6 +1,10 @@
-# open or close all network active vents depending on a vent command setting
-# RespondToInternal should be changed if this active vent set is considered internal or external.
-# intended use would have a memory / ic housing mirrored from a controlling network that wants to command two sets of multiple active vents.
+# open or close all network active vents depending on
+#  a vent command setting
+# RespondToInternal should be changed if this active
+#  vent set is considered internal or external.
+# intended use would have a memory / ic housing
+#  mirrored from a controlling network that wants to
+#  command two sets of multiple active vents.
 alias ventCommand d0
 alias externalSensor d1
 
@@ -13,7 +17,7 @@ define OFFEXT 5
 define OFFINT 6
 
 define ACTIVEVENT -1129453144
-define RespondToInternal 1
+define RespondToInternal 0
 define CLEARCMDSLEEP 0.5
 
 alias doExhaust r8
@@ -28,7 +32,7 @@ select doOff RespondToInternal OFFINT OFFEXT
 
 main:
 yield
-l r0 doorCommand Setting
+l r0 ventCommand Setting
 beq r0 doExhaust exhaust
 beq r0 doIntake intake
 beq r0 doOff off
