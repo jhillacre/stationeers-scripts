8811573a30c1525c6df0e2f969494c8e7bb5e1ee
Joel Hillacre <joel@403forbidden.ca>
2024-07-20 21:48:33 -0600

    fix: account for condensation in fuel mix.

--- fuel-mixer-via-pumps.s
+++ fuel-mixer-via-pumps.s
@@ -28,15 +28,23 @@ bdns h2pump init
 bdns o2pump init
 bdns switch init
 
-# is the tank isnt mixed correctly, fix it.
+# If the tank isn't mixed correctly, fix it.
 l r0 fuelsensor Pressure
 beqz r0 nopressure
 pressure:
 l r2 fuelsensor RatioVolatiles
 l r3 fuelsensor RatioOxygen
+
+# normalize the ratios to account for contaminants/condensation
+# Mix the gas components as best we can; trust that a filter or some venting will fix the other.
+add r6 r2 r3
+beqz r6 nopressure
+div r2 r2 r6
+div r3 r3 r6
+
 sub r4 H2RATIO r2
 sub r5 O2RATIO r3
-# overcorrect, so it doesnt take 10000 MPa to fix.
+# overcorrect, so it takes less than 10000 MPa to fix.
 mul r5 r5 OVERCORRECT
 mul r4 r4 OVERCORRECT
 add r2 H2RATIO r4
@@ -47,7 +55,7 @@ move r2 H2RATIO
 move r3 O2RATIO
 
 temperature:
-# account for input temperature when mixing
+# Account for input temperature when mixing
 l r0 h2sensor Temperature
 mul r0 r0 r2
 l r1 o2sensor Temperature
@@ -61,16 +69,16 @@ sub PUMPMAX 1 PUMPMAX
 mul PUMPMAX PUMPMAX MAXPUMP
 max PUMPMAX PUMPMAX 1
 
-# set pump L 10-0 not 1-0
+# Set pump L 10-0, not 1-0
 mul r0 r0 PUMPMAX
 s h2pump Setting r0
 sub r0 PUMPMAX r0
 s o2pump Setting r0
 
+# Should we do something?
 l r0 switch Open
-# go, no go
 beqz r0 turnoff
-# if the lines are mixed, don't mix.
+# If the lines are mixed, don't mix them.
 l r2 h2sensor RatioVolatiles
 blt r2 1 turnoff
 l r3 o2sensor RatioOxygen
@@ -82,13 +90,13 @@ s o2pump On 0
 j start
 
 setpumps:
-# turn off if we are full
+# Turn off if we are full
 l r0 fuelsensor Pressure
 slt r0 r0 MAXPRESSURE
-# or if we dont have volatiles
+# or if we don't have volatiles
 l r1 h2sensor Pressure
 sgt r1 r1 100
-# or if we dont have oxygen
+# or if we don't have oxygen
 l r2 o2sensor Pressure
 sgt r2 r2 100
 and r0 r0 r1
