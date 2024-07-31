25ada59c19fa24ca72cb563209a12dac0722dc3d
Joel Hillacre <joel@403forbidden.ca>
2024-07-30 22:47:56 -0600

    refactor: remove unused pump maxs, reducing getting wires crossed on registers..

--- air-ratio-control.s
+++ air-ratio-control.s
@@ -4,14 +4,12 @@ alias N2Pump d1
 alias O2Pump d2
 alias ExhaustPump d3
 alias ExhaustActiveVent d4
-
 define GASSENSORHASH HASH("StructureGasSensor")
 define TARGETCO2 0.01
 define TARGETN2 0.69
 define TARGETO2 0.30
 define PRESSURETARGET 149
 define PRESSUREMIN 101
-
 alias CO2Ratio r0
 alias N2Ratio r1
 alias O2Ratio r2
@@ -20,17 +18,13 @@ alias N2PumpSetting r4
 alias O2PumpSetting r5
 alias ExhaustPumpSetting r6
 alias RoomPressure r7
-alias CO2PumpMax r8
-alias N2PumpMax r9
-alias O2PumpMax r10
-alias ExhaustPumpMax r11
-# pumpSettingPD args
-alias MaxPumpSetting r12
-alias CurrentRatio r13
-alias TargetRatio r14
-alias ScaledSetting r15
-alias LastError r16 # !!!
-
+alias Proportional r9
+alias Derivative r10
+alias CurrError r11
+alias LastError r12 # hold the popped value
+alias CurrentRatio r13 # pumpSettingPD args
+alias TargetRatio r14 # pumpSettingPD args
+alias ScaledSetting r15# pumpSettingPD return
 init:
 yield
 bdns CO2Pump init
@@ -38,14 +32,11 @@ bdns N2Pump init
 bdns O2Pump init
 bdns ExhaustPump init
 bdns ExhaustActiveVent init
-l CO2PumpMax CO2Pump Maximum
-l N2PumpMax N2Pump Maximum
-l O2PumpMax O2Pump Maximum
-l ExhaustPumpMax ExhaustPump Maximum
 s ExhaustActiveVent Mode 1
 s ExhaustActiveVent On 0
 s ExhaustActiveVent Lock 0
-
+push 0
+brlt sp 4 -1
 loop:
 yield
 lb CO2Ratio GASSENSORHASH RatioCarbonDioxide 0
@@ -53,8 +44,7 @@ lb N2Ratio GASSENSORHASH RatioNitrogen 0
 lb O2Ratio GASSENSORHASH RatioOxygen 0
 lb RoomPressure GASSENSORHASH Pressure 0
 blt RoomPressure PRESSURETARGET underPressure
-# above 149 kPa
-move ExhaustPumpSetting ExhaustPumpMax
+move ExhaustPumpSetting 100 # above 149 kPa
 move CO2PumpSetting 0
 move N2PumpSetting 0
 move O2PumpSetting 0
@@ -63,65 +53,56 @@ j setPumps
 underPressure:
 s ExhaustActiveVent On 0
 bgt RoomPressure PRESSUREMIN inRange
-# under 101 kPa
-move ExhaustPumpSetting 0
+move ExhaustPumpSetting 0 # under 101 kPa
 move CO2PumpSetting 0
-move N2PumpSetting N2PumpMax
-move O2PumpSetting O2PumpMax
+move N2PumpSetting 100
+move O2PumpSetting 100
 j setPumps
-
 inRange:
-move MaxPumpSetting CO2PumpMax
-move CurrentRatio CO2Ratio
+move CurrentRatio CO2Ratio # between
 move TargetRatio TARGETCO2
+move sp 1
 jal pumpSettingPD
 move CO2PumpSetting ScaledSetting
-
-move MaxPumpSetting N2PumpMax
 move CurrentRatio N2Ratio
 move TargetRatio TARGETN2
+move sp 2
 jal pumpSettingPD
 move N2PumpSetting ScaledSetting
-
-move MaxPumpSetting O2PumpMax
 move CurrentRatio O2Ratio
 move TargetRatio TARGETO2
+move sp 3
 jal pumpSettingPD
 move O2PumpSetting ScaledSetting
-
-move MaxPumpSetting ExhaustPumpMax
 move CurrentRatio RoomPressure
 move TargetRatio PRESSURETARGET
+move sp 4
 jal pumpSettingPD
 move ExhaustPumpSetting ScaledSetting
-
 setPumps:
 s CO2Pump Setting CO2PumpSetting
 snez r15 CO2PumpSetting
 s CO2Pump On r15
-
 s N2Pump Setting N2PumpSetting
 snez r15 N2PumpSetting
 s N2Pump On r15
-
 s O2Pump Setting O2PumpSetting
 snez r15 O2PumpSetting
 s O2Pump On r15
-
 s ExhaustPump Setting ExhaustPumpSetting
 snez r15 ExhaustPumpSetting
 s ExhaustPump On r15
 j loop
-
-pumpSettingPD: # use r13 / r14 for internal math after getting their value
-sub r13 TargetRatio CurrentRatio # error
-sub r14 r13 LastError # derivative
-mul r13 r13 100 # p gain
-mul r14 r14 10 # d gain
-add ScaledSetting r13 r14
-move LastError r13
-mul ScaledSetting ScaledSetting 100 # <0.01 = 0
+pumpSettingPD:
+pop LastError
+sub CurrError TargetRatio CurrentRatio
+sub Derivative CurrError LastError
+mul Proportional CurrError 100 # p gain
+mul Derivative Derivative 10 # d gain
+add ScaledSetting Proportional Derivative
+push CurrError # update last error
+mul ScaledSetting ScaledSetting 1000 # <0.001 = 0
 round ScaledSetting ScaledSetting
-div ScaledSetting ScaledSetting 100
+div ScaledSetting ScaledSetting 1000
 max ScaledSetting ScaledSetting 0
 j ra
