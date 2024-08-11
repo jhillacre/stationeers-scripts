548a7b89e1e15b71e3726d826e21a4e9a6660047
Joel Hillacre <joel@403forbidden.ca>
2024-08-10 20:41:40 -0600

    fix: chemistry fail & lines too long for in-game editor.

--- fill-nitrogen-for-condensation.s
+++ fill-nitrogen-for-condensation.s
@@ -1,29 +1,31 @@
-# fills from a reserve N2 network to a cooling loop network for condensation in the pipe.
+# fills from a reserve N network to a cooling loop
+# network for condensation in the pipe.
 # doesn't deal with the resulting liquid.
 define LS HASH("StructureLogicSwitch")
 alias CoolingPA d0
 alias ReservePA d1
 alias TVP d2 # point default towards the cooling loop
-define N2BOILINGTEMP 75 # Stationeers N2 is built difference
-define N2BOILINGPRESSURE 100
-define N2MINCONDENSATIONTEMP 45 # 40, safety
-define N2MINCONDENSATIONPRESSURE 10 # 6.3, safety
-define N2MAXLIQUIDPRESSURE 5500 # 6000, safety
-define N2LATENTHEAT 500
+# Stationeers N is built different
+define NBOILINGTEMP 75
+define NBOILINGPRESSURE 100
+define NMINCONDENSATIONTEMP 45 # 40, safety
+define NMINCONDENSATIONPRESSURE 10 # 6.3, safety
+define NMAXLIQUIDPRESSURE 5500 # 6000, safety
+define NLATENTHEAT 500
 define RGASCONSTANT 8.3144
 define MAXSTRESSFROMLIQUID 50 # %, 100% is bad
-alias CoolingGasTemperature r0
-alias CoolingGasPressure r1
-alias CoolingGasMoles r2
-alias CoolingLoopLiquid r3
-alias CoolingLoopVolume r4
-alias ReserveGasTemperature r5
-alias ReserveGasPressure r6
-alias ReserveGasMoles r7
-alias ReserveTankVolume r8
-alias CoolingCondensationPressure r9
-alias ReserveCondensationPressure r10
-alias CoolingLoopStress r11
+alias CGT r0 # CoolingGasTemperature
+alias CGP r1 # CoolingGasPressure
+alias CGM r2 # CoolingGasMoles
+alias CLL r3 # CoolingLoopLiquid
+alias CLV r4 # CoolingLoopVolume
+alias RGT r5 # ReserveGasTemperature
+alias RGP r6 # ReserveGasPressure
+alias RGM r7 # ReserveGasMoles
+alias RTV r8 # ReserveTankVolume
+alias CCP r9 # CoolingCondensationPressure
+alias RCP r10 # ReserveCondensationPressure
+alias CLS r11 # CoolingLoopStress
 alias Control r12
 alias LastError r13
 init:
@@ -39,44 +41,44 @@ s TVP Setting 0
 move LastError 0
 next:
 yield
-l CoolingGasTemperature CoolingPA Temperature
-l CoolingGasPressure CoolingPA Pressure
-l CoolingGasMoles CoolingPA TotalMoles
-l CoolingLoopLiquid CoolingPA VolumeOfLiquid
-l CoolingLoopVolume CoolingPA Volume
-l ReserveGasTemperature ReservePA Temperature
-l ReserveGasPressure ReservePA Pressure
-l ReserveGasMoles ReservePA TotalMoles
-l ReserveTankVolume ReservePA Volume
+l CGT CoolingPA Temperature
+l CGP CoolingPA Pressure
+l CGM CoolingPA TotalMoles
+l CLL CoolingPA VolumeOfLiquid
+l CLV CoolingPA Volume
+l RGT ReservePA Temperature
+l RGP ReservePA Pressure
+l RGM ReservePA TotalMoles
+l RTV ReservePA Volume
 lb r14 LS Open 2 # safety lockout
 beqz r14 withdraw # if any closed
-move r15 CoolingGasTemperature
+move r15 CGT
 jal calculateCondensationPressure
-move CoolingCondensationPressure r15
-move r15 ReserveGasTemperature
+move CCP r15
+move r15 RGT
 jal calculateCondensationPressure
-move ReserveCondensationPressure r15
-mul CoolingLoopStress CoolingLoopLiquid 5000
-div CoolingLoopStress CoolingLoopStress CoolingLoopVolume
-blt CoolingGasTemperature N2MINCONDENSATIONTEMP withdraw
-ble ReserveGasPressure ReserveCondensationPressure withdraw
+move RCP r15
+mul CLS CLL 5000
+div CLS CLS CLV
+blt CGT NMINCONDENSATIONTEMP withdraw
+ble RGP RCP withdraw
 jal calculateAllInPressure
-bgt CoolingCondensationPressure r15 withdraw
-bgt CoolingLoopStress MAXSTRESSFROMLIQUID lowerPressure
+bgt CCP r15 withdraw
+bgt CLS MAXSTRESSFROMLIQUID lowerPressure
 raisePressure:
-move r14 N2MAXLIQUIDPRESSURE
+move r14 NMAXLIQUIDPRESSURE
 j setDevices
 lowerPressure:
-move r14 CoolingCondensationPressure
+move r14 CCP
 j setDevices
 setDevices:
-sub r14 r14 CoolingGasPressure # error
+sub r14 r14 CGP # error
 sub Control r14 LastError # derivative
 move LastError r14
 mul r14 r14 500 # p gain
 mul Control Control 100 # d gain
 add Control Control r14 # control
-div Control Control ReserveGasPressure
+div Control Control RGP
 mul Control Control 100
 round Control Control
 bnez Control on
@@ -99,21 +101,22 @@ bnez r1 withdraw
 j next
 calculateCondensationPressure:
 div r14 1 r15 # 1/CurrentTemperature
-div r15 1 N2BOILINGTEMP
+div r15 1 NBOILINGTEMP
 sub r15 r15 r14
-div r14 N2LATENTHEAT RGASCONSTANT
+div r14 NLATENTHEAT RGASCONSTANT
 mul r15 r15 r14
 exp r15 r15
-mul r15 N2BOILINGPRESSURE r15
+mul r15 NBOILINGPRESSURE r15
 j ra
 calculateAllInPressure:
-add r15 ReserveGasMoles CoolingGasMoles
-mul r14 ReserveGasMoles ReserveGasTemperature
-# clobber ReserveCondensationPressure, low on registers, don't need it until next time.
-mul ReserveCondensationPressure CoolingGasMoles CoolingGasTemperature
-add r14 r14 ReserveCondensationPressure
+add r15 RGM CGM
+mul r14 RGM RGT
+# clobber RCP, low on registers,
+# don't need it until next time.
+mul RCP CGM CGT
+add r14 r14 RCP
 div r14 r14 r15
 mul r15 r15 RGASCONSTANT
 mul r15 r15 r14
-div r15 r15 CoolingLoopVolume
+div r15 r15 CLV
 j ra
